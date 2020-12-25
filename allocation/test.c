b15c8adf5dbc9e40253a259cfa49476b32c217bc
tungwingho <dongyonghao17111766@gmail.com>
2020-12-26 00:04:58 +0800

    test

--- allocation/test.c
+++ allocation/test.c
@@ -2,6 +2,11 @@
 
 #include "dyh.h"
 
+typedef struct {
+    int x;
+    double y;
+} node;
+
 int main(int argc, char const *argv[]) {
     int n;
     scanf("%d", &n);
@@ -13,5 +18,13 @@ int main(int argc, char const *argv[]) {
         printf("%d ", num[i]);
     }
     printf("\n");
+
+    node *nd = (node *)dyh_malloc(sizeof(node) * n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d%lf", &nd[i].x, &nd[i].y);
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d %.2lf\n", nd[i].x, nd[i].y);
+    }
     return 0;
 }
